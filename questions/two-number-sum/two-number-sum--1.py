d0db595d9cf52be9ec2eb0252d24b627ad1c6e5c
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2022-06-24 13:58:14 -0400

    Create two-number-sum--1.py

---
+++ questions/two-number-sum/two-number-sum--1.py
@@ -0,0 +1,19 @@
+# basic solution
+# O(n²)
+def twoNumberSum(array, targetSum):
+    # Write your code here.
+    results = []
+
+    # iterate over dictionary
+    for x in array:
+        for y in array:
+            if array.index(x) != array.index(y):
+                if x + y == targetSum:
+                    if x not in results:
+                        results.append(x)
+                    if y not in results:
+                        results.append(y)
+
+    return(results)
+    
+    pass
